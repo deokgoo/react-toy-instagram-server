c2781053e31da381e942412758a09b1da0c49d10
deokgoo <kkddgg1001@gmail.com>
2020-08-19 21:10:59 +0900

    graphql scehema codebase

--- src/server.js
+++ src/server.js
@@ -1,21 +1,13 @@
 require('dotenv').config();
 import { GraphQLServer } from 'graphql-yoga';
+import logger from 'morgan';
+import schema from './schema';
 
 const { PORT } = process.env || 4000;
 
-const typeDefs = `
-  type Query {
-    hello: String!
-  }
-`;
+const server = new GraphQLServer({ schema });
 
-const resolvers = {
-  Query: {
-    hello: () => 'hihi',
-  }
-}
-
-const server = new GraphQLServer({ typeDefs, resolvers });
+server.express.use(logger('dev'))
 
 server.start({ port: PORT }, () => {
   console.log(`Server Running Port ${PORT}`);

054a36aabc3f67325b097781bbc311d7dd6cd159
deokgoo <kkddgg1001@gmail.com>
2020-09-01 18:08:06 +0900

    revison

--- src/api/sayGoodBye/sayGoodBye.js
+++ src/api/sayGoodBye/sayGoodBye.js
@@ -1,8 +1,7 @@
 export default {
   Query: {
-    sayGoodbye_1: () => 'bye',
-    sayGoodbye2: () => 'bye',
-    sayGoodbye3: () => 'bye'
+    sayGoodbye_2: () => 'bye',
+    sayGoodbye3: () => 'bye',
   }
   // test를 시작합니다.
 }
